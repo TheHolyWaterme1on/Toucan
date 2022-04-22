edb8ef1397dad1d590cdf2b89721958a38c68cee
TheHolyWatermelon <73319858+TheHolyWaterme1on@users.noreply.github.com>
2022-04-22 16:15:20 -0500

    Changed variable name to avoid redefining keyword

--- src/cogs/queries.py
+++ src/cogs/queries.py
@@ -9,20 +9,20 @@ class queries:
         prefix string
     )""")
 
-    def db_set(id, prefix):
-        c.execute("INSERT INTO guilds VALUES (:id, :prefix)", {'id':id, 'prefix':prefix})
+    def db_set(gid, prefix):
+        c.execute("INSERT INTO guilds VALUES (:id, :prefix)", {'id':gid, 'prefix':prefix})
 
-    def db_get(id):
+    def db_get(gid):
         with connection:
-            c.execute("SELECT * FROM guilds WHERE id=:id", {'id' : id})
+            c.execute("SELECT * FROM guilds WHERE id=:id", {'id' : gid})
             return c.fetchone()
         
-    def db_del(id):
+    def db_del(gid):
         with connection:
-            c.execute("DELETE from guilds where id={}".format(id))
+            c.execute("DELETE from guilds where id={}".format(gid))
 
-    def db_update(id, prefix):
+    def db_update(gid, prefix):
         with connection:
-            c.execute("UPDATE guilds SET prefix=:prefix WHERE id=:id", {'id' : id, 'prefix' : prefix})
+            c.execute("UPDATE guilds SET prefix=:prefix WHERE id=:id", {'id' : gid, 'prefix' : prefix})
 
 connection.commit()
